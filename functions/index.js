597451b9c93e6d02616c9cde65b41284abc989a7
Michael Hansen <michaelandhansen@gmail.com>
2020-09-27 18:22:11 -0400

    added first user routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+app.get("/", async (req, res) => {
+    const snapshot = await admin.firestore().collection("users").get();
+    let users = [];
+    snapshot.forEach((doc) => {
+        let id = doc.id;
+        let data = doc.data();
+        users.push({ id, ...data });
+    });
+    res.status(200).send(JSON.stringify(users));
+});
+
+app.post("/", async (req, res) => {
+    const user = req.body;
+    await admin.firestore().collection("users").add(user);
+    res.status(201).send();
+});
+
+exports.user = functions.https.onRequest(app);
