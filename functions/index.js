62ca6b959248a0a67612473058882b8930dce468
Michael Hansen <michaelandhansen@gmail.com>
2020-09-29 17:18:51 -0400

    added register form validations

--- functions/index.js
+++ functions/index.js
@@ -37,6 +37,19 @@ const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
 
 // < --------------------------------------- >
+//   UTILITIES
+// < --------------------------------------- >
+
+const testEmpty = (str) => {
+    str.trim() === "" ? true : false;
+};
+
+// const testEmail = (email) => {
+//     const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     email.match(emailRegEx) ? true : false;
+// };
+
+// < --------------------------------------- >
 //   ROUTING
 // < --------------------------------------- >
 
@@ -84,22 +97,79 @@ app.delete("/users/:id", async (req, res) => {
 // SIGNUP
 
 app.post("/signup", (req, res) => {
+    // prep new user payload from form
     const newUser = {
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
         userName: req.body.userName,
     };
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    let userToken, userId;
+    let inputErrors = {};
+
+    // test each field
+    if (testEmpty(newUser.email)) {
+        inputErrors.email = "Email field is required";
+    }
+    // else if (!testEmail(newUser.email)) {
+    //     inputErrors.email = "Email is not a valid address";
+    // }
+    if (testEmpty(newUser.password)) {
+        inputErrors.password = "Password is required";
+    }
+    if (newUser.password !== newUser.confirmPassword) {
+        inputErrors.password = "Passwords do not match";
+    }
+    if (testEmpty(newUser.userName)) {
+        inputErrors.userName = "Username is required";
+    }
+
+    // short circuit the rest of the operation if our
+    // errors object has accumulated any errors
+    Object.keys(inputErrors).length > 0 && res.status(400).json(inputErrors);
+    // check if username is available
+    db.doc(`/users/${newUser.userName}`)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                return res
+                    .status(400)
+                    .json({ userName: "this user name is already taken" });
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(
+                        newUser.email,
+                        newUser.password
+                    );
+            }
+        })
         .then((data) => {
-            return res.status(201).json({
-                message: `user ${data.user.uid} signed up successfully`,
-            });
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            userToken = token;
+            // create new user doc after validation
+            const userCred = {
+                userName: newUser.userName,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId: userId,
+            };
+            return db.doc(`/users/${newUser.userName}`).set(userCred);
+        })
+        .then((data) => {
+            return res.status(201).json({ userToken });
         })
         .catch((error) => {
-            return res.status(json({ error: error.code }));
+            if (error.code === "auth/email-already-in-use") {
+                return res
+                    .status(400)
+                    .json({ email: "this email is already registered" });
+            } else {
+                return res.status(500).json({ error: error.code });
+            }
         });
 });
 
