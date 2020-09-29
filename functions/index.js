591f788e8d61d42fb983b8be221dd79da3058f85
Michael Hansen <michaelandhansen@gmail.com>
2020-09-29 15:31:34 -0400

    auth route set up

--- functions/index.js
+++ functions/index.js
@@ -2,16 +2,17 @@
 //   DEPENDENCIES
 // < --------------------------------------- >
 
+require("dotenv").config();
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const userRoute = express();
+const app = express();
 
 // < --------------------------------------- >
 //   MIDDLEWARE
 // < --------------------------------------- >
 
-userRoute.use(cors({ origin: true }));
+app.use(cors({ origin: true }));
 
 // < --------------------------------------- >
 //   INITIALIZERS
@@ -21,12 +22,26 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
 // < --------------------------------------- >
 //   ROUTING
 // < --------------------------------------- >
 
 // <- GET ALL users ->
-userRoute.get("/", async (req, res) => {
+app.get("/users", async (req, res) => {
     const snapshot = await db.collection("users").get();
     let users = [];
     snapshot.forEach((doc) => {
@@ -37,8 +52,15 @@ userRoute.get("/", async (req, res) => {
     res.status(200).send(JSON.stringify(users));
 });
 
+// <- CREATE NEW user ->
+app.post("/users", async (req, res) => {
+    const user = req.body;
+    await db.collection("users").add(user);
+    res.status(201).send();
+});
+
 // <- SHOW ONE user ->
-userRoute.get("/:id", async (req, res) => {
+app.get("/users/:id", async (req, res) => {
     const snapshot = await db.collection("users").doc(req.params.id).get();
     const userId = snapshot.id;
     const userData = snapshot.data();
@@ -46,72 +68,65 @@ userRoute.get("/:id", async (req, res) => {
     res.status(200).send(JSON.stringify({ id: userId, ...userData }));
 });
 
-// <- CREATE NEW user ->
-userRoute.post("/", async (req, res) => {
-    const user = req.body;
-    await db.collection("users").add(user);
-    const newBlog = { name: req.body.userName };
-    generateBlog(newBlog);
-    res.status(201).send();
-});
-
-// <- REGISTER NEW user ->
-userRoute.post("/register", (req, res) => {
-    admin
-        .auth()
-        .createUser({
-            email: req.body.email,
-            password: req.body.password,
-        })
-        .then((userRecord) => {
-            console.log(userRecord.uid);
-            return db.collection("users").add({
-                uid: userRecord.uid,
-                userName: req.body.userName,
-            });
-        })
-        .then(res.status(201).send())
-        .catch((error) => {
-            console.log("Error creating new user:", error);
-        });
-});
-
 // <- UPDATE user ->
-userRoute.put("/:id", async (req, res) => {
+app.put("/users/:id", async (req, res) => {
     const body = req.body;
     await db.collection("users").doc(req.params.id).update(body);
     res.status(200).send();
 });
 
 // <- DELETE user ->
-userRoute.delete("/:id", async (req, res) => {
+app.delete("/users/:id", async (req, res) => {
     await db.collection("users").doc(req.params.id).delete();
     res.status(200).send();
 });
 
-exports.user = functions.https.onRequest(userRoute);
+// SIGNUP
 
-// < --------------------------------------- >
-//   CLOUD TRIGGERS
-// < --------------------------------------- >
+app.post("/signup", (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userName: req.body.userName,
+    };
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then((data) => {
+            return res.status(201).json({
+                message: `user ${data.user.uid} signed up successfully`,
+            });
+        })
+        .catch((error) => {
+            return res.status(json({ error: error.code }));
+        });
+});
 
-// account signup
-exports.newUserSignup = functions.auth.user().onCreate((user) => {
-    console.log("user created", user.email, user.uid);
-    return db.collection("blogs").add({
-        owner: user.uid,
-        name: "testing",
+// POSTS
+
+// <- GET ALL posts ->
+app.get("/posts", async (req, res) => {
+    const snapshot = await db.collection("posts").get();
+    let posts = [];
+    snapshot.forEach((doc) => {
+        let id = doc.id;
+        let data = doc.data();
+        posts.push({ id, ...data });
     });
+    res.status(200).send(JSON.stringify(posts));
 });
 
-// account delete
-exports.deleteUser = functions.auth.user().onDelete((user) => {
-    const doc = db.collection("users").doc(user.uid);
-    return doc.delete();
+app.post("/posts", (req, res) => {
+    const newPost = {
+        postBody: req.body.postBody,
+        userName: req.body.userName,
+    };
+    return db.collection("posts").add(newPost).then(res.status(200).send());
 });
 
-// for background triggers you must return a promise/value
+// < --------------------------------------- >
+//   EXPORTS
+// < --------------------------------------- >
 
-// < --------------------------------------- >
-//   UTILITY ACTIONS
-// < --------------------------------------- >
+exports.api = functions.https.onRequest(app);
